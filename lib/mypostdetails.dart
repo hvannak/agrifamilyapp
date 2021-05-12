c6677dd8821169191fd70c7ba6fa1e9a3b904529
hvannak <vannak2010@gmail.com>
2021-05-12 11:57:09 +0700

    add post details

--- lib/mypostdetails.dart
+++ lib/mypostdetails.dart
@@ -1,6 +1,8 @@
+import 'package:agrifamilyapp/Helpers/constants.dart';
 import 'package:agrifamilyapp/Widgets/mainwidget.dart';
 import 'package:agrifamilyapp/models/postdisplaymodel.dart';
 import 'package:agrifamilyapp/modules/myhomefunc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyPostDetails extends StatefulWidget {
@@ -21,7 +23,6 @@ class _MyPostDetailsState extends State<MyPostDetails> {
 
   @override
   void initState() {
-    print(widget._posts);
     super.initState();
   }
 
@@ -30,51 +31,142 @@ class _MyPostDetailsState extends State<MyPostDetails> {
     return Scaffold(
       appBar: AppBar(title: Text('Details')),
       body: Container(
-        child: FutureBuilder(
-          future: fetchPostImages(context,widget._posts.id!),
-          builder: (BuildContext context, AsyncSnapshot snapshot){
-            if (snapshot.data == null) {
-              return Container(
-                  child: Center(
-                      child: SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              )));
-            } else {
-              return Container(
-                child:Column(
-                  children: [
-                    Container(
-                      height: 180,
-                      child: Image.memory(
-                        reInstantiateImageCodec(
-                            snapshot.data[0].image.data),
-                        fit: BoxFit.cover,
+          child: FutureBuilder(
+        future: fetchPostImages(context, widget._posts.id!),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container(
+                child: Center(
+                    child: SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            )));
+          } else {
+            return Column(
+              children: <Widget>[
+                Container(
+                    height: 200,
+                    child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (snapshot.data == null) {
+                            return CupertinoActivityIndicator();
+                          } else {
+                            return Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Card(
+                                shadowColor: Colors.blue,
+                                child: Container(
+                                  child: Image.memory(
+                                    reInstantiateImageCodec(
+                                        snapshot.data[index].image.data),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                        })),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      ColoredBox(
+                        color: Colors.purple.shade100,                     
+                        child: Row(children: <Widget>[
+                          Container(
+                            height: 35,
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Center(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.content_copy,color: Colors.green[600],),
+                                    SizedBox(width: 20,),
+                                    Text(widget._posts.title,style: headertextStyle,)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ]),
                       ),
-                    ),
-                    ListTile(
-                      leading: Icon(
-                        Icons.info,
-                        color: Colors.green,
+                      ListTile(
+                        title: Text(widget._posts.description),
                       ),
-                      title: Text(widget._posts.title),
-                      subtitle:
-                          Text(widget._posts.category.title),
-                    ),
-                  ],
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      ColoredBox(
+                        color: Colors.yellow.shade100,                     
+                        child: Row(children: <Widget>[
+                          Container(
+                            height: 35,
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Center(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.email,color: Colors.green[600],),
+                                    SizedBox(width: 20,),
+                                    Text('Contact',style: headertextStyle,)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ]),
+                      ),
+                      ListTile(
+                        title: Text(widget._posts.phone!),
+                        subtitle: Text((widget._posts.email == null ? '' : widget._posts.email)!),
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      ColoredBox(
+                        color: Colors.cyan.shade100,                     
+                        child: Row(children: <Widget>[
+                          Container(
+                            height: 35,
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Center(
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.home,color: Colors.green[600],),
+                                    SizedBox(width: 20,),
+                                    Text('Address',style: headertextStyle,)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ]),
+                      ),
+                      ListTile(
+                        title: Text(widget._posts.location!)
+                      ),
+                    ],
+                  ),
                 )
-              );
-            }
-          },
-        )
-        
-      ),
+              ],
+            );
+          }
+        },
+      )),
       bottomNavigationBar: BottomNavigationBar(
         items: widgetBottomNav,
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,      
+        onTap: _onItemTapped,
       ),
     );
   }
